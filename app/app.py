1ace929dd322c95a8ee825a92effc1c7a7e71e70
Rupali Arlekar <rupa1277@gmail.com>
2026-08-18 05:53:59 +0530

    Trigger CI pipeline check

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+# CI/CD demo application
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Version 1 - deployed via Argo CD!"
+    return "changes made to Version 1 - deployed via Argo CD! To check"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
